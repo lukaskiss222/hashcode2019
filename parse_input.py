c24dd6e8b09ac488ad8d5673df64620f9e54e0ca
Jozef Martiš <mjjozef@gmail.com>
2019-02-28 19:40:14 +0100

    Zaobalenie nacitavania vstupu

--- parse_input.py
+++ parse_input.py
@@ -1,15 +1,15 @@
+def load():
+    n = int(input())
 
-n = int(input())
+    photos = []
 
-photos = []
+    for i in range(n):
+        tupla = []
+        inpt = input().split()
 
-for i in range(n):
-    tupla = []
-    inpt = input().split()
+        tupla.append(1 if inpt[0] == 'H' else 0)
 
-    tupla.append(1 if inpt[0] == 'H' else 0)
+        for j in range(2, len(inpt)):
+            tupla.append(inpt[j])
 
-    for j in range(2, len(inpt)):
-        tupla.append(inpt[j])
-
-    photos.append(tuple(tupla))
+        photos.append(tuple(tupla))
